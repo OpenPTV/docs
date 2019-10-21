d395add72de9600ee16a57d1455c4c72aa8ac5ea
Alex Liberzon <alexlib@users.noreply.github.com>
2019-10-21 19:45:08 +0300

    moved pdfs to the right location

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,8 +17,8 @@ import sys, os
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-sys.path.append(os.path.abspath('../../pyptv_gui/'))
-sys.path.append(os.path.abspath('../../src_c/'))
+sys.path.append(os.path.abspath('/home/user/Downloads/pyptv/pyptv/pyptv/'))
+sys.path.append(os.path.abspath('/home/user/openptv/openptv/py_bind/liboptv/src/'))
 
 
 # -- General configuration -----------------------------------------------------
