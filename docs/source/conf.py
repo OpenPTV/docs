cec08ef90189225e319e2e19a43f1feda17c5da8
alexlib <alex.liberzon@gmail.com>
2025-04-14 17:50:01 +0300

    chnage of theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -104,7 +104,33 @@ html_theme_options = {
     "light_css_variables": {
         "color-brand-primary": "#0078C1",
         "color-brand-content": "#0078C1",
+        "color-background-primary": "#f8f9fa",
+        "color-background-secondary": "#f0f2f4",
+        "color-foreground-primary": "#333333",
+        "color-foreground-secondary": "#5a5c63",
+        "color-admonition-background": "#e7f2fa",
     },
+    "dark_css_variables": {
+        "color-brand-primary": "#2b9cd8",
+        "color-brand-content": "#2b9cd8",
+        "color-background-primary": "#131416",
+        "color-background-secondary": "#1a1c1e",
+        "color-foreground-primary": "#f8f9fa",
+        "color-foreground-secondary": "#9ca0a5",
+        "color-admonition-background": "#1c2631",
+    },
+    "footer_icons": [
+        {
+            "name": "GitHub",
+            "url": "https://github.com/OpenPTV/openptv-python",
+            "html": """
+                <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path>
+                </svg>
+            """,
+            "class": "",
+        },
+    ],
 }
 
 # Theme options are theme-specific and customize the look and feel of a theme
@@ -124,17 +150,23 @@ html_theme_options = {
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = None
+html_logo = "../images/openptv_logo.png"
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-#html_favicon = None
+html_favicon = "../images/openptv_logo.png"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = []
+html_static_path = ['_static']
+
+# These paths are either relative to html_static_path
+# or fully qualified paths (eg. https://...)
+html_css_files = [
+    'custom.css',
+]
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
